744e5ab00ac6579995a7e2d943fa275e0e753782
mrPbunny <marco.hensen@hotmail.com>
2020-01-06 14:26:11 +0100

    profile fragment reads data from firebase

--- app/src/main/java/com/diegor/bani/model/User.java
+++ app/src/main/java/com/diegor/bani/model/User.java
@@ -9,8 +9,10 @@ import java.util.HashMap;
 public class User {
 
     private String fullName;
-    private String photo;
+    private String Telephone;
     private String email;
+    private String Adres;
+    private String photo;
     private HashMap<String, Object> timestampJoined;
 
     public User() {
@@ -22,27 +24,63 @@ public class User {
      *
      * @param timestampJoined
      */
-    public User(String mFullName, String mPhoneNo, String mEmail, HashMap<String, Object> timestampJoined) {
+    public User(String mFullName, String mPhoto, String mEmail, HashMap<String, Object> timestampJoined) {
         this.fullName = mFullName;
-        this.photo = mPhoneNo;
+        this.photo = mPhoto;
         this.email = mEmail;
         this.timestampJoined = timestampJoined;
     }
 
+    public User(String mFullName, String mPhoneNo, String mEmail, String mAdres){
 
+        if(mAdres.length() == 0){
+            mAdres = "Adres";
+        }
+
+        if(mPhoneNo.length() == 0){
+            mPhoneNo = "Telefoon";
+        }
+
+        this.fullName = mFullName;
+        this.Telephone = mPhoneNo;
+        this.email = mEmail;
+        this.Adres = mAdres;
+    }
+
+    //getters
     public String getFullName() {
         return fullName;
     }
-
     public String getPhoto() {
         return photo;
     }
-
     public String getEmail() {
         return email;
     }
-
+    public String getAdres() { return Adres; }
+    public String getTelephone() { return Telephone; }
     public HashMap<String, Object> getTimestampJoined() {
         return timestampJoined;
+    }
+
+    //setters
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setAdres(String Adres) {
+        this.Adres = Adres;
+    }
+    public void setPhone(String Telephone) {
+        this.Telephone = Telephone;
+    }
+
+    public void setTimestampJoined(HashMap<String, Object> timestampJoined) {
+        this.timestampJoined = timestampJoined;
     }
 }
--- app/src/main/java/com/diegor/bani/profiel/ProfielFragment.java
+++ app/src/main/java/com/diegor/bani/profiel/ProfielFragment.java
@@ -1,43 +1,46 @@
 package com.diegor.bani.profiel;
 
+import androidx.constraintlayout.solver.widgets.Snapshot;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.diegor.bani.MainActivity;
-import com.diegor.bani.NavDrawerActivity;
 import com.diegor.bani.R;
-import com.diegor.bani.utils.SharedPrefManager;
-import com.google.android.gms.auth.api.Auth;
+import com.diegor.bani.model.User;
+import com.firebase.client.Query;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class ProfielFragment extends Fragment {
 
@@ -51,6 +54,12 @@ public class ProfielFragment extends Fragment {
     private String mMail;
     private String mTel;
 
+    private String adres;
+    private String phone;
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference();
+
 
     public static ProfielFragment newInstance() {
         ProfielFragment fragment = new ProfielFragment();
@@ -62,30 +71,73 @@ public class ProfielFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        Context c = getActivity().getApplicationContext();
-
-        String name = getArguments().getString("name");
-        String mail = getArguments().getString("mail");
+        final String name = getArguments().getString("name");
+        final String mail = getArguments().getString("mail");
         String image = getArguments().getString("photo");
+
+        final DatabaseReference usersRef = ref.child("users").child("users/" + mail.replace('.', ','));
 
         View view = inflater.inflate(R.layout.profiel_fragment, container, false);
         View navView = inflater.inflate(R.layout.nav_header, container, false);
 
-        TextView nameView = (TextView) view.findViewById(R.id.name);
+        final EditText adresView = (EditText) view.findViewById(R.id.adres);
+        final EditText telView = (EditText) view.findViewById(R.id.tel);
+
+        ValueEventListener userListener = new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                User user = dataSnapshot.getValue(User.class);
+
+                adres = user.getAdres();
+                Log.i("current data Adres: ", adres);
+                phone = user.getTelephone();
+                Log.i("current data phone: ", phone);
+
+                adresView.setText(adres);
+                telView.setText(phone);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError){
+                Log.w(TAG, "loadUser:onCancelled", databaseError.toException());
+            }
+        };
+        usersRef.addValueEventListener(userListener);
+
+        Context c = getActivity().getApplicationContext();
+
+        final TextView nameView = (TextView) view.findViewById(R.id.name);
         nameView.setText(name);
 
-        EditText email = (EditText) view.findViewById(R.id.mail);
+        TextView email = (TextView) view.findViewById(R.id.mail);
         email.setText(mail);
 
         CircleImageView viewer = (CircleImageView) navView.findViewById(R.id.profileImage);
         ImageView photoView = (ImageView) view.findViewById(R.id.photo);
-        //photoView.setImageDrawable(viewer.getDrawable());
 
         Picasso.with(c)
                 .load(image)
                 .placeholder(android.R.drawable.sym_def_app_icon)
                 .error(android.R.drawable.sym_def_app_icon)
                 .into(photoView);
+
+        Button updater = (Button) view.findViewById(R.id.wijzigButton);
+        updater.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mName = name;
+                mAdres = adresView.getText().toString();
+                mMail = mail.replace('.', ',');
+                mTel = telView.getText().toString();
+
+                User user = new User(mName, mTel, mMail, mAdres);
+
+                Map<String, Object> users = new HashMap<>();
+                users.put("Adres", mAdres);
+                users.put("Telephone", mTel);
+
+                usersRef.child(mMail).updateChildren(users);
+            }
+        });
 
         return view;
     }
